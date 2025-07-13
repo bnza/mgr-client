1ed4bb3e192dcd1516262da6654fb4a06f8c9d6e
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2025-07-13 15:13:56 +0200

    refactor(client): remove openapi plugin and relocate API spec fetching
    Eliminate the unused `openapi` plugin and migrate the API spec fetching logic directly into the `app.vue` component. Ensure consistency by updating the store usage to handle OpenAPI spec retrieval and error handling within the component.

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { OpenAPIV3_1 } from 'openapi-types'
+
 const { ready } = storeToRefs(useOpenApiStore())
 const name = computed(() => (ready.value ? 'default' : 'empty'))
 
@@ -6,6 +8,17 @@ const { isAuthenticated } = useAppAuth()
 const { invalidateQueries } = useQueryCache()
 
 watch(isAuthenticated, () => invalidateQueries())
+
+await $fetch<OpenAPIV3_1.Document>('/api/docs.jsonopenapi', {
+  baseURL: useNuxtApp().$config.public.apiBaseUrl,
+})
+  .then((response) => {
+    const { specInternal } = storeToRefs(useOpenApiStore())
+    specInternal.value = response
+  })
+  .catch((e) => {
+    console.error('miao', e)
+  })
 </script>
 
 <template>
