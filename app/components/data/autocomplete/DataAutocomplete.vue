1d1f0ef294e4d31de5da2a1fbed4ca67adc9caaf
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2025-07-28 14:48:11 +0200

    refactor(client): simplify model handling in DataAutocomplete components
    Update `DataAutocomplete` and `DataAutocompleteSite` components to use a single string model type instead of complex object-based models. Refactor `v-model` bindings and remove unnecessary computed logic for reactive handling.

--- app/components/data/autocomplete/DataAutocomplete.vue
+++ app/components/data/autocomplete/DataAutocomplete.vue
@@ -2,7 +2,7 @@
 import type { ApiResourcePath } from '~/utils/consts/resources'
 import useAutocompleteQuery from '~/composables/queries/useAutocompleteQuery'
 
-const model = defineModel<string | { '@id': string }>()
+const model = defineModel<string>()
 const props = defineProps<{
   path: ApiResourcePath
   itemTitle: string
@@ -10,23 +10,11 @@ const props = defineProps<{
 
 const search = ref('')
 const { items, asyncStatus } = useAutocompleteQuery(props.path, search)
-const iri = computed({
-  get() {
-    return isString(model.value) ? model.value : model.value?.['@id']
-  },
-  set(value) {
-    if (isString(model.value) || typeof model.value === 'undefined') {
-      model.value = value
-    } else {
-      model.value['@id'] = value!
-    }
-  },
-})
 </script>
 
 <template>
   <v-autocomplete
-    v-model="iri"
+    v-model="model"
     no-filter
     item-value="@id"
     :items="items || []"
